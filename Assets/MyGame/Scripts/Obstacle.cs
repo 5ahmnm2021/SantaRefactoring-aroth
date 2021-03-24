e8e15d8b212d13f02274820fb9e53831463023f9
alessiaRoth <alessia.roth@htl.salzburg.ac.at>
2021-03-24 15:17:14 +0100

    fixed formatting

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -14,25 +14,20 @@ private void Awake()
     // Update is called once per frame
     private void Update()
     {
-    //if obstacle's position x is < -15f it will be destroyed
-    if(transform.position.x < -15f)
-    {
-        Destroy(gameObject);
+        //if obstacle's position x is < -15f it will be destroyed
+        if (transform.position.x < -15f)
+        {
+            Destroy(gameObject);
         }
         //if obstacle's position x is < -15f it will be destroyed
         if (transform.position.x > 15f)
         {
             Destroy(gameObject);
         }
-
     }
 
-
-
-private void FixedUpdate()
-{
-
-    rb.velocity = Vector2.left * MoveSpeed;
-
-}
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.left * MoveSpeed;
+    }
 }
