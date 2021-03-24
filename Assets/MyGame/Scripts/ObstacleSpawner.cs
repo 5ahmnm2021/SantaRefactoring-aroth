8b5b8bf16a676fe3e2152e14f4458f35a971b580
alessiaRoth <alessia.roth@htl.salzburg.ac.at>
2021-03-24 15:29:31 +0100

    added access modifier and deleted comment

--- Assets/MyGame/Scripts/ObstacleSpawner.cs
+++ Assets/MyGame/Scripts/ObstacleSpawner.cs
@@ -20,8 +20,7 @@ public class ObstacleSpawner : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         StartCoroutine("Spawn");
     }
